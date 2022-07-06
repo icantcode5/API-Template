39d11163fddba187c9710519cc1540e54bc0ef46
Carlos Sosa <carlossosa54321@gmail.com>
2022-07-05 18:55:31 -0700

    added my api template app

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const MongoClient = require('mongodb').MongoClient 
+require('dotenv').config()
+//const PORT = 8000
+
+let db
+let dbConnectionString = process.env.DB_STRING
+let dbName = 'Star-Wars-Quotes'
+let collection
+
+MongoClient.connect(dbConnectionString)
+.then(client => {
+    console.log('Connected To DB')
+    db = client.db(dbName)
+    collection = db.collection('quotes')
+})
+
+app.set('view engine','ejs')
+app.use(express.static('Public'))
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+app.use(cors())
+
+
+
+app.listen(process.env.PORT || PORT, () => {
+    console.log('Server is up and running ')
+})
